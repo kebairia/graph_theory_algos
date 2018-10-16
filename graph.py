aa6675132780572d7829f8aef5f657f7e37a2dd8
Kebairia_Zakaria <4.kebairia@gmail.com>
2018-10-16 17:34:53 +0100

    adding some changes

--- graph.py
+++ graph.py
@@ -1,19 +1,11 @@
-#!/bin/env python
-#AUTHOR: Kebairia Zakaria
-#EMAIL : z.kebairia@esi-sba.dz
-#             _  _                     _        _
-#    __ _  __| |(_)    _ __ ___   __ _| |_ _ __(_)_  __
-#   / _` |/ _` || |   | '_ ` _ \ / _` | __| '__| \ \/ /
-#  | (_| | (_| || |   | | | | | | (_| | |_| |  | |>  <
-#   \__,_|\__,_|/ |___|_| |_| |_|\__,_|\__|_|  |_/_/\_\
-#             |__/_____|
-#
+!/bin/env python
 class Graph:
     def __init__(self,Graph_init):
         self.edge={}
         for keys,values in Graph_init.items():
              for value in values:
                  self.addEdge(keys,value);
+
     def addVertex(self,v):
         if v not in self.edge:
             self.edge[v] = []
@@ -28,8 +20,10 @@ class Graph:
         if v not in self.edge[u]:
             self.edge[u].append(v)
     
+    def calculateLength(self):
+        l = len(self.edge.keys())
+        return l
     
     def display(self):
         for keys,values in self.edge.items():
             print(keys,":=>",values)
-
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/bin/python
 import graph
+import basic_op as bp
 
 def main():
     Graph_init = {1:[2,3,5],
-              2:[1,4],
-              3:[1,6]};
+                  2:[1,4],
+                  3:[1,6]};
     
     g=graph.Graph(Graph_init)
     g.addVertex(1)
@@ -14,5 +15,6 @@ def main():
     g.addEdge(3,2)
     g.addEdge(6,5)
     g.display()
+    print (Graph_init.items())
 if __name__ == '__main__':
     main()
